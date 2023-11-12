a4c78536f7420efeff84539eb19f8acdc8f391b8
Evgeniia <pshenitsyna.e@gmail.com>
2023-11-12 18:28:50 +0100

    Add first main file

---
+++ main.py
@@ -0,0 +1,12 @@
+contacts = {
+    "James": {
+        "phone_number": 12345678,
+        "email": "james.big@gmail.com",
+    },
+    "Jenny": {
+         "phone_number": 12345555,
+         "email": "jenny.big@gmail.com",
+    }
+}
+
+print(contacts["James"]["email"])
