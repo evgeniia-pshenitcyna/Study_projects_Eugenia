0bb84abe84e28dcd20455f59eee69a12ac907dc3
Evgeniia <pshenitsyna.e@gmail.com>
2023-11-12 22:58:37 +0100

    add data file

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-contacts = {
-    "James": {
-        "phone_number": 12345678,
-        "email": "james.big@gmail.com",
-    },
-    "Jenny": {
-         "phone_number": 12345555,
-         "email": "jenny.big@gmail.com",
-    }
-}
+from data import contacts
 
 print(contacts["James"]["email"])
+
+
+def add_numbers(n1, n2):
+    total = n1 + n2
+    return total
+
+
+print(add_numbers(4, 3))
