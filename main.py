78efb7026d7ef14f80641c7f085738dca78eb699
Evgeniia <pshenitsyna.e@gmail.com>
2023-11-12 23:10:18 +0100

    Change the name of the function

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from data import contacts
 print(contacts["James"]["email"])
 
 
-def add(n1, n2):
+def add_numbers(n1, n2):
     total = n1 + n2
     return total
 
 
-print(add(4, 3))
+print(add_numbers(4, 3))
