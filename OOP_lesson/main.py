0678765f538678e07214fc9ca79464a6a178f7a2
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2023-11-19 00:40:16 +0100

    add pretty table

--- OOP_lesson/main.py
+++ OOP_lesson/main.py
@@ -1,11 +1,19 @@
-from turtle import Turtle, Screen
+#from turtle import Turtle, Screen
 
-eugene = Turtle()
-print(eugene)
-eugene.shape("turtle")
-eugene.color("DeepPink2")
-eugene.forward(100)
+#eugene = Turtle()
+#print(eugene)
+#eugene.shape("turtle")
+#eugene.color("DeepPink2")
+#eugene.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon name", ["Pikachu", "Skirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table)
+
