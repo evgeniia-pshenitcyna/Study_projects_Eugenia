b023c3b181020ea07d425b8b270f0d7fd5f07b9f
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2023-11-19 00:47:34 +0100

    add pretty table

--- OOP_lesson/main.py
+++ OOP_lesson/main.py
@@ -15,5 +15,6 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon name", ["Pikachu", "Skirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "r"
 print(table)
 
