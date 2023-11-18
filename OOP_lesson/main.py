a590754390a2e060a32d998bada4fc6cb71e4a32
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2023-11-19 00:18:01 +0100

    add a bit

--- OOP_lesson/main.py
+++ OOP_lesson/main.py
@@ -2,6 +2,9 @@ from turtle import Turtle, Screen
 
 eugene = Turtle()
 print(eugene)
+eugene.shape("turtle")
+eugene.color("DeepPink2")
+eugene.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
