956bd5768d43718363da28e09feecf95e7d31e93
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2023-11-20 22:44:01 +0100

    save function in class

--- day_17_OOP/main.py
+++ day_17_OOP/main.py
@@ -4,7 +4,15 @@ class User:
         self.name = name
         self.age = age
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("Dasha", 7)
-print(f"Name: {user_1.name}, age: {user_1.age}, followers: {user_1.followers}")
+user_2 = User("Ivan", 12)
+user_2.follow(user_1)
+
+print(user_2.following, user_1.followers)
