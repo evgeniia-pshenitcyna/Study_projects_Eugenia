5de009434dcd99f660c1ba6ba97ab80dec6ae2bd
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2023-11-20 22:30:33 +0100

    init function

--- day_17_OOP/main.py
+++ day_17_OOP/main.py
@@ -1,5 +1,9 @@
 class User:
-    pass
+
+    def __init__(self, user_name, user_age):
+        self.name = user_name
+        self.age = user_age
 
 
-user_1 = User()
+user_1 = User("Dasha", 7)
+print(user_1.name, user_1.age)
