e07fd037c380155b8fcc97211cd784c34afd282d
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-01-04 23:36:02 +0100

    improved design

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,22 +1,25 @@
 from turtle import Turtle
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 
 
 class Snake:
 
     def __init__(self):
-        for position in starting_position:
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
             my_turtle = Turtle("square")
             my_turtle.color("white")
             my_turtle.penup()
             my_turtle.goto(position)
-            segments.append(my_turtle)
+            self.segments.append(my_turtle)
 
     def move(self):
-        for seg_num in range(len(segments) - 1, 0, -1):
-            new_x = segments[seg_num - 1].xcor()
-            new_y = segments[seg_num - 1].ycor()
-            segments[seg_num].goto(new_x, new_y)
-        segments[0].forward(20)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
