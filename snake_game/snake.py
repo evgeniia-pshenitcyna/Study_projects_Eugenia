a186fe55a9cb49c3a9fcc4e36b0b4ec35ff22db7
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-01-05 00:19:48 +0100

    finishing onkey functionality

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 
 class Snake:
@@ -8,6 +12,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -22,20 +27,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def right(self):
-        for turtle in self.segments:
-            turtle.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
     def up(self):
-        for turtle in self.segments:
-            turtle.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def left(self):
-        for turtle in self.segments:
-            turtle.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def down(self):
-        for turtle in self.segments:
-            turtle.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
