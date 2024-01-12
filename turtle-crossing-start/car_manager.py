1649f4e26ea5240e9df6f5be109691fbf640f422
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-01-13 00:30:01 +0100

    cross turtle task one

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -1,7 +1,22 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(1, 2)
+        self.penup()
+        self.setheading(180)
+        self.color(random.choice(COLORS))
+        x_pos = 280
+        y_pos = random.randint(-280, 280)
+        self.goto(x_pos, y_pos)
+
+    def move(self):
+        self.forward(MOVE_INCREMENT)
--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -8,7 +8,22 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player_1 = Player()
+car_1 = CarManager()
+car_2 = CarManager()
+car_3 = CarManager()
+car_4 = CarManager()
+
+screen.listen()
+screen.onkey(player_1.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_1.move()
+    car_2.move()
+    car_3.move()
+    car_4.move()
+
+screen.exitonclick()
