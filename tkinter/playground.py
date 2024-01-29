d4a8da404962bf8b91dcdc0425e48e07b651485e
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-01-29 23:38:59 +0100

    playground exercises

--- tkinter/playground.py
+++ tkinter/playground.py
@@ -1,5 +1,6 @@
 # unlimited positional arguments
 def add(*args):
+    print(args)
     result = 0
     for n in args:
         result += n
@@ -10,8 +11,29 @@ add(5, 2, 1)
 
 
 # keyword arguments
-def calculate(**kwargs):
-    print(type(kwargs))
+def calculate(n, **kwargs):
+    #print(type(kwargs))
+    #print(kwargs["add_n"])
+    #for key, value in kwargs.items():
+        #print(key)
+        #print(value)
+    n += kwargs["add_n"]
+    n *= kwargs["multiply"]
+    print(n)
 
 
-calculate(add_n=3, multiply=5)
+calculate(2, add_n=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kwargs):
+        #this way it would require argument to be passed. Get returns None if argument not passed
+        #self.make = kwargs["make"]
+        #self.model = kwargs["model"]
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+
+
+my_car = Car(make='Nissan', model='GT-R')
+print(my_car.model)
