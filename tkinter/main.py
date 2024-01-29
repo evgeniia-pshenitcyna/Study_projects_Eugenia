ac4ff667f0981684036ede186ff75bca90cea272
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-01-30 00:16:43 +0100

    add more to tkinter

--- tkinter/main.py
+++ tkinter/main.py
@@ -4,7 +4,25 @@ window = tkinter.Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
+# Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side='right')
+my_label.pack()
+
+my_label["text"] = "New text"
+my_label.config(text="Cat")
+
+
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+# Button
+my_button = tkinter.Button(text="Click me", command=button_clicked)
+my_button.pack()
+
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
 
 window.mainloop()
