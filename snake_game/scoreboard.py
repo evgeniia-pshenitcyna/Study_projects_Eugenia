f5a0602c6ddf887ae61cbe69e17b4c7a7b1871a7
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-02-14 23:40:35 +0100

    Save changes to snake game

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -12,14 +12,18 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.score = 0
+        self.high_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game over.",  align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.clear()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,10 +1,10 @@
 from turtle import Turtle
-STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
-RIGHT = 0
 LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -15,8 +15,18 @@ class Snake:
         self.head = self.segments[0]
 
     def create_snake(self):
-        for position in STARTING_POSITION:
+        for position in STARTING_POSITIONS:
             self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -25,29 +35,25 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
-
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
-
-    def left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
 
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
-    def add_segment(self, position):
-        my_turtle = Turtle("square")
-        my_turtle.color("white")
-        my_turtle.penup()
-        my_turtle.goto(position)
-        self.segments.append(my_turtle)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
-    def extend(self):
-        # add new segment to the snake
-        self.add_segment(self.segments[-1].position())
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
