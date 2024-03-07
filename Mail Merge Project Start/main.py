6491cf71fa68bf17223652a48baf0cae2dfb1ad2
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-03-07 09:29:34 +0100

    Finalised letters

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -4,13 +4,14 @@ with open("Input/Letters/starting_letter.txt") as file:
 
 with open("Input/Names/invited_names.txt") as file:
     names_list = []
-    for n in range(1, 8):
+    for n in range(1, 9):
         name = file.readlines(n)
-        names_list.append(str(name).strip("['\\n']"))
+        names_list.append(str(name).strip("[\\\n\\n']"))
     print(names_list)
 
-#for name in names_list:
-    #letter = letter_text.replace("[name]", name)
-    #txt = "letter_" + name + ".txt"
-    #with open(f"Output/ReadyToSend/{txt}", mode="w") as file:
-        #file.write(letter)
+for name in names_list:
+    name = name.strip('""')
+    letter = letter_text.replace("[name]", name)
+    txt = "letter_" + name + ".txt"
+    with open(f"Output/ReadyToSend/{txt}", mode="w") as file:
+        file.write(letter)
