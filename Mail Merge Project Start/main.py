edcc246ddd703009f0a26501002545e8c106ceaf
e.pshenitcyna <evgeniia.pshenitcyna@deliveryhero.com>
2024-03-07 09:16:32 +0100

    remove comments

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,11 +1,3 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-
-#f = open("demofile.txt", "r")
-#print(f.readlines())
-
 with open("Input/Letters/starting_letter.txt") as file:
     letter_text = file.read()
     print(letter_text)
@@ -17,8 +9,8 @@ with open("Input/Names/invited_names.txt") as file:
         names_list.append(str(name).strip("['\\n']"))
     print(names_list)
 
-for name in names_list:
-    letter = letter_text.replace("[name]", name)
-    txt = "letter_" + name + ".txt"
-    with open(f"Output/ReadyToSend/{txt}", mode="w") as file:
-        file.write(letter)
+#for name in names_list:
+    #letter = letter_text.replace("[name]", name)
+    #txt = "letter_" + name + ".txt"
+    #with open(f"Output/ReadyToSend/{txt}", mode="w") as file:
+        #file.write(letter)
